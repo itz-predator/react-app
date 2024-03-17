be756c78c4aeb658b020bd5ad6b3b404b98c6628
Rohit Singh <70831064+itz-predator@users.noreply.github.com>
2024-03-17 10:35:06 +0000

    first push

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import octocat from './octocat.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={octocat} alt="octocat" />
+        <p>
+          <h1>Hi! There Its Me Testing OTC</h1>
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
